80d279071fe7ebdb6ec99f2f721a3dc6e4f0217c
GideokLee <76939190+GideokLee@users.noreply.github.com>
2022-04-11 22:39:52 +0900

    Project Setup

---
+++ Walker_Server/src/main/java/com/walker/api/WalkerServerApplication.java
@@ -0,0 +1,13 @@
+package com.walker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkerServerApplication.class, args);
+	}
+
+}
---
+++ Walker_Server/src/test/java/com/walker/api/WalkerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
